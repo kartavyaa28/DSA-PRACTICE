ab35da0a2a70178c850e4a9f6917ab0c2ccb5ce9
Kartavya Agrahari <kartavyaagrahari@gmail.com>
2026-07-24 18:19:00 +0530

    Time: 0 ms (100%), Space: 95.2 MB (61.98%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0, j = 0;
-        vector<int> result;
+        if(nums2.size() < nums1.size())
+           return findMedianSortedArrays(nums2, nums1);
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int low=0;
+        int high=n1;
+        while(low <= high)
+        {
+            int cut1 = (low+high)/2;
+            int cut2 = (n1+n2+1)/2 - cut1;
 
-        // Merge two sorted arrays
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] <= nums2[j])
-                result.push_back(nums1[i++]);
+            int left1 = cut1==0?INT_MIN:nums1[cut1-1];
+            int left2 = cut2==0?INT_MIN:nums2[cut2-1];
+
+            int right1 = cut1==n1?INT_MAX:nums1[cut1];
+            int right2 = cut2==n2?INT_MAX:nums2[cut2];
+
+            if(left1 <= right2 && left2 <= right1)
+            {
+                if((n1+n2)%2 == 0)
+                {
+                    return(max(left1,left2)+min(right1,right2))/2.0; // median of even length
+                }
+                else
+                    return max(left1,left2); // median of odd length
+            }
+
+            else if(left1 > right2)
+            {
+                high = cut1-1;
+            }
             else
-                result.push_back(nums2[j++]);
-        }
-        while (i < nums1.size())
-            result.push_back(nums1[i++]);
-        while (j < nums2.size())
-            result.push_back(nums2[j++]);
+            {
+                low = cut1+1;
+            }
 
-        // Find median
-        int n = result.size();
-        if (n % 2 == 0) {
-            return (result[n / 2 - 1] + result[n / 2]) / 2.0;
-        } else {
-            return result[n / 2];
-        }
+
+        }   
+        return 0.0;
+        
+      
     }
 };
