a1a585b542113080d4d57ad19a832d846239f21a
Kartavya Agrahari <kartavyaagrahari@gmail.com>
2026-07-30 18:34:35 +0530

    Time: 0 ms (100%), Space: 8.1 MB (68.79%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        
-        int n = s.length();
-        if (n == 1) {
-            if (s == goal)
-                return true;
+        if(s.length() != goal.length())return false;
+        if(s.length() == 1)
+        {
+            if(s == goal)return true;
             return false;
+
         }
-        for (int i = 0; i < n; i++) {
-            char first = s[0];
-            for (int j = 1; j < n; j++) {
-                s[j - 1] = s[j];
-            }
-            s[n - 1] = first;
-            
-            if (s == goal) {
+        string newS = s + s;
+        for(int i = 0; i< newS.length(); i++)
+        {
+            if(newS.substr(i,goal.length()) == goal)
+            {
                 return true;
             }
         }
-
         return false;
+        
     }
 };
