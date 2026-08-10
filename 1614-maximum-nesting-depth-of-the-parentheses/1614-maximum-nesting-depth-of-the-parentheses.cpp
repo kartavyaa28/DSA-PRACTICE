3beca19094482c8bc188deabbbb91060e37726df
Kartavya Agrahari <kartavyaagrahari@gmail.com>
2026-08-10 18:46:39 +0530

    Time: 0 ms (100%), Space: 8.3 MB (86.93%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int count = 0;
+        int maxP = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(')
+            {
+                count++;
+            }
+            else if(s[i] == ')')
+            {
+                maxP = max(maxP,count);
+                count--;
+            }
+        }
+
+        return maxP;
+        
+    }
+};
