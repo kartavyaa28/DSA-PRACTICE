9db6d8eafd54ccc109e199d39affa16ba9f82106
Kartavya Agrahari <kartavyaagrahari@gmail.com>
2026-09-09 15:17:48 +0530

    Time: 3 ms (95.61%), Space: 12.3 MB (56.53%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -12,9 +12,13 @@ public:
         // copy the next node val into given node val
         node->val = node->next->val;
 
+        ListNode* temp = node->next; // using to remove completely next node , prevention from memory leak
+
         //now point current node to its next of next node , it will reduce its next node
 
         node->next = node->next->next;
+
+        delete temp;
 
         
         
