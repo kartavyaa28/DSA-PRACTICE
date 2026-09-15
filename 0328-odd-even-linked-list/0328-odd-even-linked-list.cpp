cda90433b0b3f923f77fd236d0f917ff2016d00b
Kartavya Agrahari <kartavyaagrahari@gmail.com>
2026-09-15 22:14:37 +0530

    Time: 0 ms (100%), Space: 15.7 MB (27.49%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -16,37 +16,22 @@ public:
         vector<int>even;
 
         ListNode* temp = head;
-        int size = 0;
+        int idx = 1;
         while(temp != nullptr)
         {
-            size++;
-            temp = temp->next;
-        }
-        temp = head;
-        int i = 1;
-        while(temp != nullptr && i <= size)
-        {
-            if(i % 2 == 0){
-            even.push_back(temp->val);
-            i++;}
-            else
-            {
-            odd.push_back(temp->val);
-            i++;}
+            if(idx % 2 == 0) even.push_back(temp->val);
+            else odd.push_back(temp->val);
+            idx++;
             temp=temp->next;
 
         }
-        for(int i = 0 ; i < even.size() ; i++)
-        {
-            odd.push_back(even[i]);
-        }
+        for(int x : even)odd.push_back(x);
         temp = head;
         int j = 0;
         while(temp != nullptr && j < odd.size())
         {
-            temp->val = odd[j];
-            temp=temp->next;
-            j++;
+            temp->val = odd[j++];
+            temp = temp->next;
         }
         return head;
         
